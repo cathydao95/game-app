8f2c3ea599a43aa46f2490a6bef00e038a43b504
Cathy Dao <cathydao95@gmail.com>
2023-09-06 21:36:05 -0700

    create guesses and useEffect to shuffle answers

--- client/src/components/Questions.jsx
+++ client/src/components/Questions.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-const Questions = ({ quiz }) => {
-  // declare the function
+const Questions = ({ quiz, correctAnswers }) => {
+  const [guesses, setGuesses] = useState(Array(quiz.length).fill(null));
+  const [randomizedAnswers, setRandomizedAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+
+  // RANDOMIZE ANSWERS
   const shuffle = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -9,21 +13,93 @@ const Questions = ({ quiz }) => {
     }
     return array;
   };
+
+  useEffect(() => {
+    let answers = quiz.map((quizQuestion) => {
+      const { correct_answer, incorrect_answers } = quizQuestion;
+      let answers = [...incorrect_answers, correct_answer];
+      let randomAnswers = shuffle(answers);
+      return randomAnswers;
+    });
+    setRandomizedAnswers(answers);
+  }, [quiz]);
+
+  console.log("randomAnswers", randomizedAnswers);
+
+  // WHEN USER CLICKS ON AN ANSWER, RUN SELECTGUESS FUNCTION
+  const selectGuess = (e, index) => {
+    // console.log(e.target.dataset.answer);
+    let guess = e.target.dataset.answer;
+    // console.log(index);
+    // setGuesses((prevGuesses) => {
+    //   const newGuesses = [...prevGuesses];
+    //   newGuesses[index] = guess;
+    //   return newGuesses;
+    // });
+    setGuesses((prevGuesses) => {
+      const newGuesses = [...prevGuesses];
+      newGuesses[index] = { guess: guess };
+      return newGuesses;
+    });
+  };
+
+  // WHEN USER SUBMITS QUIZ, CHECK ANSWERS
+  const checkQuizAnswers = () => {
+    if (guesses.every((guess) => guess !== null)) {
+      console.log(guesses);
+      const checkGuesses = guesses.map((guess, index) => {
+        console.log(
+          "tdoes this work",
+          guess,
+          correctAnswers[index],
+          guess === correctAnswers[index]
+        );
+        return { ...guess, isCorrect: guess.guess === correctAnswers[index] };
+      });
+      setGuesses(checkGuesses);
+      setScore(
+        guesses.reduce((acc, guess, index) => {
+          if (guess.guess === correctAnswers[index]) {
+            return acc + 1;
+          }
+          return acc;
+        }, 0)
+      );
+    } else {
+      console.log("please answer every question");
+    }
+  };
+
+  console.log("GUESSES", guesses);
   return (
     <div>
-      <h1>test</h1>
       <div>
-        {quiz.map((quizQuestion) => {
-          const { question, correct_answer, incorrect_answers } = quizQuestion;
-          let answers = [...incorrect_answers, correct_answer];
-          let randomizedAnswers = shuffle(answers);
+        {/* get index of question to set correct answer index */}
+        {randomizedAnswers.map((answers, index) => {
           return (
-            <div key={question}>
-              <h3>{question}</h3>
+            <div key={index}>
+              <h3>{quiz[index].question}</h3>
               <div>
                 <ul>
-                  {randomizedAnswers.map((answer) => {
-                    return <button key={answer}>{answer}</button>;
+                  {answers.map((answer) => {
+                    // if answer is equal to index in guess array, isSelected is true -- to use for classNames
+                    const isSelected = guesses[index]?.guess === answer;
+                    const isCorrect =
+                      guesses[index]?.isCorrect &&
+                      guesses[index]?.guess === answer;
+
+                    return (
+                      <button
+                        key={answer}
+                        data-answer={answer}
+                        className={`${isSelected ? "gold" : ""} ${
+                          isCorrect ? "green" : ""
+                        }  `}
+                        onClick={(event) => selectGuess(event, index)}
+                      >
+                        {answer}
+                      </button>
+                    );
                   })}
                 </ul>
               </div>
@@ -31,6 +107,8 @@ const Questions = ({ quiz }) => {
           );
         })}
       </div>
+      <button onClick={checkQuizAnswers}>Submit Answers</button>
+      <h3>Score: {score}</h3>
     </div>
   );
 };
