d0204a54776655cd07811f6a9d6b8f149569fb7b
Cathy Dao <cathydao95@gmail.com>
2023-09-07 10:56:04 -0700

    add classes

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -3,10 +3,17 @@ import "./Form.css";
 
 const Form = ({ selectOptions, getQuizQuestions }) => {
   return (
-    <div>
+    <div className="formContainer">
       <form className="settingsForm">
-        <label htmlFor="category">Select a Category:</label>
-        <select id="category" name="category" onChange={selectOptions}>
+        <label className="categoryTitle" htmlFor="category">
+          Select a Category:
+        </label>
+        <select
+          className="quizOption"
+          id="category"
+          name="category"
+          onChange={selectOptions}
+        >
           <option value="general">General Knowledge</option>
           <option value="television">Entertainment: Television</option>
           <option value="animals">Animals</option>
@@ -14,13 +21,22 @@ const Form = ({ selectOptions, getQuizQuestions }) => {
           <option value="history">History</option>
           <option value="any">Any Category</option>
         </select>
-        <label htmlFor="difficulty">Select Difficulty</label>
-        <select id="difficulty" name="difficulty" onChange={selectOptions}>
+        <label className="categoryTitle" htmlFor="difficulty">
+          Select Difficulty
+        </label>
+        <select
+          className="quizOption"
+          id="difficulty"
+          name="difficulty"
+          onChange={selectOptions}
+        >
           <option value="easy">Easy</option>
           <option value="medium">Medium</option>
           <option value="hard">Hard</option>
         </select>
-        <button onClick={getQuizQuestions}>Start Playing</button>
+        <button className="startBtn btn" onClick={getQuizQuestions}>
+          Start Playing
+        </button>
       </form>
     </div>
   );
